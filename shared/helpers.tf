09d10ebff773b81c6838b4bf6ee6665527b85895
Brett Curtis <brett@osinfra.io>
2025-08-02 20:39:06 -0400

    Cleanup and add release workflow (#15)

--- shared/helpers.tf
+++
@@ -1,6 +0,0 @@
-# OpenTofu Core Child Module Helpers (osinfra.io)
-# https://github.com/osinfra-io/terraform-core-helpers
-
-module "helpers" {
-  source = "github.com/osinfra-io/terraform-core-helpers//child?ref=v0.1.2"
-}
